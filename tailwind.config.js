c11114b242e434c8da8baf114a46cad573ff66dc
Matej Buocik <matej.buocik@gmail.com>
2024-09-14 21:26:00 +0200

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "selector",
+  content: ["./www/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- www/index.js
+++ www/index.js
@@ -1,12 +1,24 @@
-const toggleMode = () => {
-    const body = document.body;
-    const img = document.getElementById("mode")
+const html = document.documentElement;
+const img = document.getElementById("mode-switch");
 
-    if (!body.className) {
-        body.className = "dark";
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    html.classList.add("dark");
+    img.src = "icons/dark.svg";
+} else {
+    html.classList.remove("dark");
+    img.src = "icons/light.svg";
+}
+
+const toggleMode = () => {
+    if (!html.classList.contains("dark")) {
+        // Next mode is dark
+        localStorage.theme = "dark";
         img.src = "icons/dark.svg";
     } else {
-        body.className = "";
+        localStorage.theme = "light";
         img.src = "icons/light.svg";
     }
+    html.classList.toggle("dark");
 };
+
+img.addEventListener("click", toggleMode);
